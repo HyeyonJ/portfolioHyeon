fa9d72d22bc0e992aa4fe075453d40d3f5ca7766
HyeyeonJ <michaelvia29@gmail.com>
2023-03-23 18:43:50 +0900

    first commit

---
+++ src/main/java/project/portfolioHyeon/PortfolioHyeonApplication.java
@@ -0,0 +1,13 @@
+package project.portfolioHyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioHyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioHyeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/portfolioHyeon/PortfolioHyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package project.portfolioHyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioHyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
